4a8509b116fb1aabfd7b0d8138c8f44e99bececf
Pieter Delobelle <pieter.delobelle@gmail.com>
2020-12-12 16:11:16 +0100

    HomeView: location marker

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -6,7 +6,7 @@ import {
   Platform,
   PermissionsAndroid,
 } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 
 const GEOLOCATION_OPTIONS = {
@@ -63,7 +63,7 @@ export default class HomeView extends React.PureComponent {
   componentWillUnmount() {
     this.mounted = false;
     if (this.watchID) {
-      navigator.geolocation.clearWatch(this.watchID);
+      Geolocation.clearWatch(this.watchID);
     }
   }
 
@@ -77,21 +77,67 @@ export default class HomeView extends React.PureComponent {
             longitude: 3.71667,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
-          }}
-        />
+          }}>
+          <Marker
+            anchor={{x: 0.5, y: 0.5}}
+            style={styles.mapMarker}
+            {...this.props}
+            coordinate={this.state.myPosition}>
+            <View style={styles.container}>
+              <View style={styles.markerHalo} />
+              
+              <View style={styles.marker}/>
+            </View>
+            {this.props.children}
+          </Marker>
+        </MapView>
       </View>
     );
   }
 }
 
+const SIZE = 12;
+const HALO_RADIUS = 7;
+const ARROW_SIZE = 7;
+const ARROW_DISTANCE = 6;
+const HALO_SIZE = SIZE + HALO_RADIUS;
+const HEADING_BOX_SIZE = HALO_SIZE + ARROW_SIZE + ARROW_DISTANCE;
+
 const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
   map: {
     ...StyleSheet.absoluteFillObject,
+  },
+  container: {
+    width: HEADING_BOX_SIZE,
+    height: HEADING_BOX_SIZE,
+  },
+  mapMarker: {
+    zIndex: 1000,
+  },
+  markerHalo: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    top: 0,
+    left: 0,
+    width: HALO_SIZE,
+    height: HALO_SIZE,
+    borderRadius: Math.ceil(HALO_SIZE / 2),
+    margin: (HEADING_BOX_SIZE - HALO_SIZE) / 2,
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 0,
+      width: 0,
+    },
+  },
+  marker: {
+    justifyContent: 'center',
+    backgroundColor: 'blue',
+    width: SIZE,
+    height: SIZE,
+    borderRadius: Math.ceil(SIZE / 2),
+    margin: (HEADING_BOX_SIZE - SIZE) / 2,
   },
   bubble: {
     backgroundColor: 'rgba(255,255,255,0.7)',
