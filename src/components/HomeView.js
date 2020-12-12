ae4db46275fac3c91776be292c4d428b6e01d835
Pieter Delobelle <pieter.delobelle@gmail.com>
2020-12-12 15:44:00 +0100

    Added location request to HomeView component

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -1,22 +1,87 @@
 import * as React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  PermissionsAndroid,
+} from 'react-native';
 import MapView from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
-function HomeView() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 51.05,
-          longitude: 3.71667,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-    </View>
-  );
+const GEOLOCATION_OPTIONS = {
+  enableHighAccuracy: true,
+  timeout: 20000,
+  maximumAge: 1000,
+};
+
+Geolocation.setRNConfiguration(GEOLOCATION_OPTIONS);
+
+export default class HomeView extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.mounted = false;
+    this.state = {
+      myPosition: null,
+    };
+  }
+
+  watchLocation() {
+    this.watchID = Geolocation.getCurrentPosition(
+      (position) => {
+        const myLastPosition = this.state.myPosition;
+        const myPosition = position.coords;
+        if (myPosition !== myLastPosition) {
+          this.setState({myPosition});
+        }
+      },
+      null,
+      this.props.geolocationOptions,
+    );
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    // If you supply a coordinate prop, we won't try to track location automatically
+    if (this.props.coordinate) {
+      return;
+    }
+
+    if (Platform.OS === 'android') {
+      PermissionsAndroid.requestPermission(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      ).then((granted) => {
+        if (granted && this.mounted) {
+          this.watchLocation();
+        }
+      });
+    } else {
+      this.watchLocation();
+    }
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+    if (this.watchID) {
+      navigator.geolocation.clearWatch(this.watchID);
+    }
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 51.05,
+            longitude: 3.71667,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -40,5 +105,3 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
   },
 });
-
-export default HomeView;
