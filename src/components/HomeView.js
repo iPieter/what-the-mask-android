3241656ed33721fd1b4a0444a8c3ccec27cd33d7
Pieter Delobelle <pieter.delobelle@gmail.com>
2020-12-18 12:34:20 +0100

    Home view: Component now uses background location api

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 import MapView, {Marker, Geojson} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
+import BackgroundGeolocation from 'react-native-background-geolocation';
 
 const GEOLOCATION_OPTIONS = {
   enableHighAccuracy: true,
@@ -62,6 +63,89 @@ export default class HomeView extends React.PureComponent {
       myPosition: null,
       region: null,
     };
+  }
+
+  componentWillMount() {
+    ////
+    // 1.  Wire up event-listeners
+    //
+
+    // This handler fires whenever bgGeo receives a location update.
+    BackgroundGeolocation.onLocation(this.onLocation, this.onError);
+
+    // This handler fires when movement states changes (stationary->moving; moving->stationary)
+    BackgroundGeolocation.onMotionChange(this.onMotionChange);
+
+    // This event fires when a change in motion activity is detected
+    BackgroundGeolocation.onActivityChange(this.onActivityChange);
+
+    // This event fires when the user toggles location-services authorization
+    BackgroundGeolocation.onProviderChange(this.onProviderChange);
+
+    ////
+    // 2.  Execute #ready method (required)
+    //
+    BackgroundGeolocation.ready(
+      {
+        // Geolocation Config
+        desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
+        distanceFilter: 10,
+        // Activity Recognition
+        stopTimeout: 1,
+        // Application config
+        debug: false, // <-- enable this hear sounds for background-geolocation life-cycle.
+        logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
+        stopOnTerminate: true, // <-- Allow the background-service to continue tracking when user closes the app.
+        startOnBoot: true, // <-- Auto start tracking when device is powered-up.
+        // HTTP / SQLite config
+        url: 'http://192.168.0.20/api/locations',
+        batchSync: false, // <-- [Default: false] Set true to sync locations to server in a single HTTP request.
+        autoSync: true, // <-- [Default: true] Set true to sync each location to server as it arrives.
+        headers: {
+          // <-- Optional HTTP headers
+          'X-FOO': 'bar',
+        },
+        params: {
+          // <-- Optional HTTP params
+          auth_token: 'maybe_your_server_authenticates_via_token_YES?',
+        },
+      },
+      (state) => {
+        console.log(
+          '- BackgroundGeolocation is configured and ready: ',
+          state.enabled,
+        );
+
+        if (!state.enabled) {
+          ////
+          // 3. Start tracking!
+          //
+          BackgroundGeolocation.start(function () {
+            console.log('- Start success');
+          });
+        }
+      },
+    );
+  }
+
+  // You must remove listeners when your component unmounts
+  componentWillUnmount() {
+    BackgroundGeolocation.removeListeners();
+  }
+  onLocation(location) {
+    console.log('[location] -', location);
+  }
+  onError(error) {
+    console.warn('[location] ERROR -', error);
+  }
+  onActivityChange(event) {
+    console.log('[activitychange] -', event); // eg: 'on_foot', 'still', 'in_vehicle'
+  }
+  onProviderChange(provider) {
+    console.log('[providerchange] -', provider.enabled, provider.status);
+  }
+  onMotionChange(event) {
+    console.log('[motionchange] -', event.isMoving, event.location);
   }
 
   watchLocation() {
