ce6b5edb90a34a718775f3c4c5b3df4954065d5f
Gert-Jan Bottu <gertjan.bottu@kuleuven.be>
2021-01-29 20:49:58 +0100

    Migrated to mauron85_geolocation

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -1,13 +1,12 @@
 import * as React from 'react';
-import {View, Text, StyleSheet, Settings, Platform} from 'react-native';
+import {View, Text, StyleSheet, Settings, Platform, Alert, Pressable} from 'react-native';
 import MapView, {Marker, Geojson} from 'react-native-maps';
 import {Button} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {Pressable} from 'react-native';
-import BackgroundGeolocation from 'react-native-background-geolocation';
 import {Notifications} from 'react-native-notifications';
 import 'react-native-get-random-values';
 import {v4 as uuidv4} from 'uuid';
+import BackgroundGeolocation from '@mauron85/react-native-background-geolocation';
 
 Icon.loadFont();
 
@@ -64,78 +63,110 @@ export default class HomeView extends React.PureComponent {
     );
   }
 
-  componentWillMount() {
-    ////
-    // 1.  Wire up event-listeners
-    //
-
-    // This handler fires whenever bgGeo receives a location update.
-    BackgroundGeolocation.onLocation(
-      (location) => this.onLocation(location),
-      this.onError,
-    );
-
-    // This handler fires when movement states changes (stationary->moving; moving->stationary)
-    BackgroundGeolocation.onMotionChange(this.onMotionChange);
-
-    // This event fires when a change in motion activity is detected
-    BackgroundGeolocation.onActivityChange(this.onActivityChange);
-
-    // This event fires when the user toggles location-services authorization
-    BackgroundGeolocation.onProviderChange(this.onProviderChange);
-
-    ////
-    // 2.  Execute #ready method (required)
-    //
-    BackgroundGeolocation.ready(
-      {
-        // Geolocation Config
-        desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
-        distanceFilter: 10,
-        // Activity Recognition
-        stopTimeout: 2,
-        // Application config
-        debug: false, // <-- enable this hear sounds for background-geolocation life-cycle.
-        logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
-        stopOnTerminate: false, // <-- Allow the background-service to continue tracking when user closes the app.
-        startOnBoot: true, // <-- Auto start tracking when device is powered-up.
-        // HTTP / SQLite config
-        url: 'http://192.168.0.20/api/locations',
-        batchSync: false, // <-- [Default: false] Set true to sync locations to server in a single HTTP request.
-        autoSync: true, // <-- [Default: true] Set true to sync each location to server as it arrives.
-        headers: {
-          // <-- Optional HTTP headers
-          'X-FOO': 'bar',
-        },
-        params: {
-          // <-- Optional HTTP params
-          auth_token: 'maybe_your_server_authenticates_via_token_YES?',
-        },
-      },
-      (state) => {
-        console.log(
-          '- BackgroundGeolocation is configured and ready: ',
-          state.enabled,
-        );
-
-        if (!state.enabled) {
-          ////
-          // 3. Start tracking!
-          //
-          BackgroundGeolocation.start(function () {
-            console.log('- Start success');
-          });
-        }
-      },
-    );
-  }
-
-  // You must remove listeners when your component unmounts
-  componentWillUnmount() {
-    BackgroundGeolocation.removeListeners();
-  }
-
   async componentDidMount() {
+    BackgroundGeolocation.configure({
+      desiredAccuracy: BackgroundGeolocation.HIGH_ACCURACY,
+      stationaryRadius: 10,
+      distanceFilter: 10,
+      notificationTitle: 'WhatTheMask background tracking',
+      notificationText: 'enabled',
+      debug: false,
+      startOnBoot: true,
+      stopOnTerminate: false,
+      locationProvider: BackgroundGeolocation.ACTIVITY_PROVIDER,
+      interval: 10000,
+      fastestInterval: 5000,
+      activitiesInterval: 10000,
+      stopOnStillActivity: false,
+      url: 'http://192.168.81.15:3000/location',
+      httpHeaders: {
+          'pragma': 'no-cache',
+          'cache-control': 'no-cache'
+      },
+      // customize post properties
+      // postTemplate: {
+      //   lat: '@latitude',
+      //   lon: '@longitude',
+      //   foo: 'bar' // you can also add your own properties
+      // }
+    });
+
+    BackgroundGeolocation.on('location', (location) => this.onLocation(location) );
+      // handle your locations here
+      // to perform long running operation on iOS
+      // you need to create background task
+      // BackgroundGeolocation.startTask(taskKey => {
+        // execute long running task
+        // eg. ajax post location
+        // IMPORTANT: task has to be ended by endTask
+        // BackgroundGeolocation.endTask(taskKey);
+      // });
+    // });
+
+    BackgroundGeolocation.on('stationary', (stationaryLocation) => {
+      // handle stationary locations here
+      Actions.sendLocation(stationaryLocation);
+    });
+
+    BackgroundGeolocation.on('error', (error) => {
+      console.log('[ERROR] BackgroundGeolocation error:', error);
+    });
+
+    BackgroundGeolocation.on('start', () => {
+      console.log('[INFO] BackgroundGeolocation service has been started');
+    });
+
+    BackgroundGeolocation.on('stop', () => {
+      console.log('[INFO] BackgroundGeolocation service has been stopped');
+    });
+
+    BackgroundGeolocation.on('authorization', (status) => {
+      console.log('[INFO] BackgroundGeolocation authorization status: ' + status);
+      if (status !== BackgroundGeolocation.AUTHORIZED) {
+        // we need to set delay or otherwise alert may not be shown
+        setTimeout(() =>
+          Alert.alert('App requires location tracking permission', 'Would you like to open app settings?', [
+            { text: 'Yes', onPress: () => BackgroundGeolocation.showAppSettings() },
+            { text: 'No', onPress: () => console.log('No Pressed'), style: 'cancel' }
+          ]), 1000);
+      }
+    });
+
+    BackgroundGeolocation.on('background', () => {
+      console.log('[INFO] App is in background');
+    });
+
+    BackgroundGeolocation.on('foreground', () => {
+      console.log('[INFO] App is in foreground');
+    });
+
+    BackgroundGeolocation.on('abort_requested', () => {
+      console.log('[INFO] Server responded with 285 Updates Not Required');
+
+      // Here we can decide whether we want stop the updates or not.
+      // If you've configured the server to return 285, then it means the server does not require further update.
+      // So the normal thing to do here would be to `BackgroundGeolocation.stop()`.
+      // But you might be counting on it to receive location updates in the UI, so you could just reconfigure and set `url` to null.
+    });
+
+    BackgroundGeolocation.on('http_authorization', () => {
+      console.log('[INFO] App needs to authorize the http requests');
+    });
+
+    BackgroundGeolocation.checkStatus(status => {
+      console.log('[INFO] BackgroundGeolocation service is running', status.isRunning);
+      console.log('[INFO] BackgroundGeolocation services enabled', status.locationServicesEnabled);
+      console.log('[INFO] BackgroundGeolocation auth status: ' + status.authorization);
+
+      // you don't need to check status before start (this is just the example)
+      if (!status.isRunning) {
+        BackgroundGeolocation.start(); //triggers start on start event
+      }
+    });
+
+    // you can also just start without checking for status
+    // BackgroundGeolocation.start();
+
     // Some code to prevent caching of the geojson file
     var myHeaders = new Headers();
     myHeaders.append('pragma', 'no-cache');
@@ -160,6 +191,108 @@ export default class HomeView extends React.PureComponent {
       alert('HTTP-Error: ' + response.status);
     }
   }
+
+  componentWillUnmount() {
+    // unregister all event listeners
+    BackgroundGeolocation.removeAllListeners();
+  }
+
+  // componentWillMount() {
+  //   ////
+  //   // 1.  Wire up event-listeners
+  //   //
+
+  //   // This handler fires whenever bgGeo receives a location update.
+  //   BackgroundGeolocation.onLocation(
+  //     (location) => this.onLocation(location),
+  //     this.onError,
+  //   );
+
+  //   // This handler fires when movement states changes (stationary->moving; moving->stationary)
+  //   BackgroundGeolocation.onMotionChange(this.onMotionChange);
+
+  //   // This event fires when a change in motion activity is detected
+  //   BackgroundGeolocation.onActivityChange(this.onActivityChange);
+
+  //   // This event fires when the user toggles location-services authorization
+  //   BackgroundGeolocation.onProviderChange(this.onProviderChange);
+
+  //   ////
+  //   // 2.  Execute #ready method (required)
+  //   //
+  //   BackgroundGeolocation.ready(
+  //     {
+  //       // Geolocation Config
+  //       desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
+  //       distanceFilter: 10,
+  //       // Activity Recognition
+  //       stopTimeout: 2,
+  //       // Application config
+  //       debug: false, // <-- enable this hear sounds for background-geolocation life-cycle.
+  //       logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
+  //       stopOnTerminate: false, // <-- Allow the background-service to continue tracking when user closes the app.
+  //       startOnBoot: true, // <-- Auto start tracking when device is powered-up.
+  //       // HTTP / SQLite config
+  //       url: 'http://192.168.0.20/api/locations',
+  //       batchSync: false, // <-- [Default: false] Set true to sync locations to server in a single HTTP request.
+  //       autoSync: true, // <-- [Default: true] Set true to sync each location to server as it arrives.
+  //       headers: {
+  //         // <-- Optional HTTP headers
+  //         'X-FOO': 'bar',
+  //       },
+  //       params: {
+  //         // <-- Optional HTTP params
+  //         auth_token: 'maybe_your_server_authenticates_via_token_YES?',
+  //       },
+  //     },
+  //     (state) => {
+  //       console.log(
+  //         '- BackgroundGeolocation is configured and ready: ',
+  //         state.enabled,
+  //       );
+
+  //       if (!state.enabled) {
+  //         ////
+  //         // 3. Start tracking!
+  //         //
+  //         BackgroundGeolocation.start(function () {
+  //           console.log('- Start success');
+  //         });
+  //       }
+  //     },
+  //   );
+  // }
+
+  // // You must remove listeners when your component unmounts
+  // componentWillUnmount() {
+  //   BackgroundGeolocation.removeListeners();
+  // }
+
+  // async componentDidMount() {
+  //   // Some code to prevent caching of the geojson file
+  //   var myHeaders = new Headers();
+  //   myHeaders.append('pragma', 'no-cache');
+  //   myHeaders.append('cache-control', 'no-cache');
+
+  //   var myInit = {
+  //     method: 'GET',
+  //     headers: myHeaders,
+  //   };
+
+  //   let response = await fetch(
+  //     'https://bat.ipieter.be/bat/policy/zones.geojson',
+  //     myInit,
+  //   );
+
+  //   if (response.ok) {
+  //     // if HTTP-status is 200-299
+  //     // get the response body (the method explained below)
+  //     let json = await response.json();
+  //     this.setState({geojson: json});
+  //   } else {
+  //     alert('HTTP-Error: ' + response.status);
+  //   }
+  // }
 
   onLocation(location) {
     console.log('[location] -', location);
@@ -249,15 +382,15 @@ export default class HomeView extends React.PureComponent {
   onError(error) {
     console.warn('[location] ERROR -', error);
   }
-  onActivityChange(event) {
-    console.log('[activitychange] -', event); // eg: 'on_foot', 'still', 'in_vehicle'
-  }
+  // onActivityChange(event) {
+  //   console.log('[activitychange] -', event); // eg: 'on_foot', 'still', 'in_vehicle'
+  // }
   onProviderChange(provider) {
     console.log('[providerchange] -', provider.enabled, provider.status);
   }
-  onMotionChange(event) {
-    console.log('[motionchange] -', event.isMoving, event.location);
-  }
+  // onMotionChange(event) {
+  //   console.log('[motionchange] -', event.isMoving, event.location);
+  // }
 
   inside(point, vs) {
     // ray-casting algorithm based on
