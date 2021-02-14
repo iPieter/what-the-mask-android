964ef44f0a20550f910c24b163dae54a5545f912
Pieter Delobelle <pieter.delobelle@gmail.com>
2021-02-14 13:27:52 +0100

    Welcome view A: layout for ios'

--- src/components/WelcomeViewA.js
+++ src/components/WelcomeViewA.js
@@ -1,12 +1,19 @@
 import * as React from 'react';
-import {View, Text, Image, StyleSheet, Switch} from 'react-native';
-import { Button } from 'react-native-elements';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Switch,
+  TouchableOpacity,
+} from 'react-native';
+import {Button} from 'react-native-elements';
 import SegmentedControl from '@react-native-community/segmented-control';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 Icon.loadFont();
 
-const icon = require('../../res/icon.png');
+const icon = require('../../res/welcome_screen_illustration_a.png');
 
 export default class WelcomeViewA extends React.PureComponent {
   constructor(props) {
@@ -15,41 +22,58 @@ export default class WelcomeViewA extends React.PureComponent {
   }
 
   render() {
-      return (
-              <View style={styles.container}>
-              <Text style={styles.header}>Welkom bij What the Mask!</Text>
-              <Image style={styles.icon} source={icon}/>
-              <View style={styles.bottomArea}>
-              <Button
-          onPress={() => this.navigation.navigate('Location')}
-          title="Next"
-              />
-              </View>
-              </View>
-      );
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Welkom bij 'What the Mask'!</Text>
+
+        <Image style={styles.icon} source={icon} />
+        <Text style={styles.text}>
+          Waar moet je een mondmasker dragen en waar niet? Welke regels gelden
+          waar? 'What the mask' geeft een handig overzicht voor de huidige
+          regels in Vlaanderen. Kom je in een mondmaskerzone, dan krijg je een
+          melding van de app. Be safe, wear your mask!
+        </Text>
+        <View style={styles.bottomArea}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this.navigation.navigate('Location')}>
+            <Text style={styles.buttonText}> Begin nu </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  text: {
-      color: '#fff',
-      fontSize: 30,
-      fontWeight: 'bold'
-  },
   container: {
     flex: 1,
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#426CF4',
+    height: 40,
+    borderRadius: 7,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    height: '100%',
+    margin: 10,
+    fontSize: 17,
+    fontWeight: '500',
   },
   icon: {
     justifyContent: 'center',
-    alignItems: 'center',
     resizeMode: 'contain',
-    width: '100%',
+    width: '90%',
     height: undefined,
     aspectRatio: 1,
   },
   bottomArea: {
     width: '100%',
     bottom: 0,
+    margin: '10%',
     position: 'absolute',
   },
   item: {
@@ -59,11 +83,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#444',
   },
   header: {
-    fontSize: 24,
+    marginTop: '5%',
+    color: '#2c3e50',
+    fontSize: 25,
     fontWeight: '700',
-    marginTop: 15,
-    marginHorizontal: 22,
-    marginBottom:15,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    alignItems: 'center',
   },
   title: {
     fontSize: 24,
@@ -73,7 +99,7 @@ const styles = StyleSheet.create({
     marginTop: 5,
     marginBottom: 7,
     marginHorizontal: 22,
-    color: 'grey',
+    color: '#7f8c8d',
   },
   box: {
     paddingHorizontal: 20,
